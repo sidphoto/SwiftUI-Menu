6e3718453dbe344e9c7d4b8f752260dc9e8d9733
Sidphoto <m0925726551@gmail.com>
2021-07-07 12:33:14 +0800

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by 鄭家騰 on 2021/7/7.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
